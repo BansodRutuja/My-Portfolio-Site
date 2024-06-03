bee8e58d703581e3591fb2e1590b8e56597662ab
@RBstar <132323382+BansodRutuja@users.noreply.github.com>
2024-06-03 12:17:29 +0000

    github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "RUTUJA BANSOD",
+  title: "Web Designer & ML Enthusiastic",
+  email: "rutub1910@gmail.com",
+  gitHub: "BansodRutuja",
+  instagram: "",
+  linkedIn: "Rutuja Bansod",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a IT student studying at Shri Sant Gajanan Maharaj College of Engineering,Shegaon. I am in the exciting phase of your educational journey where foundational knowledge and practical skills begin to intertwine. As a second-year IT engineering student who enjoys singing, I have a unique blend of technical and creative talents.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Canva",
+  "GitHub",
+  "Teamwork ",
+  "Adaptability",
+  "Time Management",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. Networking with peers, professors and professionals through clubs is also a key part of my growth";
 
 const About = () => {
   return (
